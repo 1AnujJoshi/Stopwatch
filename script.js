451beb7e2d670c1675f0d3c58637c6ddf8d864f5
Anuj Joshi <101502385+1AnujJoshi@users.noreply.github.com>
2022-04-09 17:14:28 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
 window.onload = function () {
 
+// variables initialised with zero at starting
 var hours = 00;
 var mins = 00;
 var secs = 00;
 var milli = 00;
 
+//getting all the elements whose innerhtml needs to be changed while running stopwatch
 var Hours = document.getElementById('hours');
 var Mins = document.getElementById('mins');
 var Secs = document.getElementById('secs');
 var Milli = document.getElementById('milli');
 
+// getting all the button by their id's 
 var btnStart = document.getElementById('btn-start');
 var btnStop = document.getElementById('btn-stop');
 var btnReset = document.getElementById('btn-reset');
+
+//variable for setting an interval
 var Interval;
 
+// start button -interval is cleared- interval is set and stopwatch starts
 btnStart.onclick = function(){
     clearInterval(Interval);
-    Interval = setInterval(startTimer, 10);
+    Interval = setInterval(Timer, 10);
 }
 
+//stop button stops the stopwatch - value gets paused- interval is cleared
 btnStop.onclick = function(){
     clearInterval(Interval);
 }
 
+//reset button - interval is cleared and all the variables are initialised to 0 and inner text is changed t0 00 as well
 btnReset.onclick = function(){
     clearInterval(Interval);
-    location.reload();   
+    hours = 0;
+    mins = 0;
+    secs = 0 ;
+    milli = 0;
+    Hours.innerHTML = "00";
+    Mins.innerHTML = "00";
+    Secs.innerHTML = "00" ;
+    Milli.innerHTML = "00";  
 }
 
-function startTimer () {
+//Timer function which controls the seconds, minutes and hours to be displayed in stopwatch
+function Timer () {
     milli++; 
     
     if(milli <= 9){
@@ -54,7 +70,7 @@ function startTimer () {
     if (secs > 9){
       Secs.innerHTML = secs;
     }
-
+    // if seconds becomes 60 then increase 1 minute and initialize seconds to 0
     if(secs > 59){
         mins++;
         Mins.innerHTML = "0" + mins;
@@ -69,7 +85,7 @@ function startTimer () {
     if( mins > 9){
         Mins.innerHTML = mins;
     }
-
+    // if minutes becomes equals to 60 initiaze it with 0 and increase hour by 1
     if(mins > 59){
         hours++;
         Hours.innerHTML = "0" + hours;
@@ -82,7 +98,5 @@ function startTimer () {
     }
   
   }
-
-
 
 }
